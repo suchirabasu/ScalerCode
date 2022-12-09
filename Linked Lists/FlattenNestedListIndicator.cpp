b0d68f4f276eee75d9277bdd1a906d103500af29
suchirabasu <suchira.basu95@gmail.com>
2022-12-09 08:11:17 +0530

    Linked list Q3

---
+++ Linked Lists/FlattenNestedListIndicator.cpp
@@ -0,0 +1,51 @@
+/*
+Q3. Flatten Nested List Iterator
+You are given a nested list of integers nestedList. Each element is either an integer or a list whose elements may also be integers or other lists. Implement an iterator to flatten it.
+
+Implement the NestedIterator class:
+
+NestedIterator(List nestedList) Initializes the iterator with the nested list nestedList.
+int next() Returns the next integer in the nested list.
+boolean hasNext() Returns true if there are still some integers in the nested list and false otherwise.
+
+Example 1:
+Input: nestedList = [[1,1],2,[1,1]]
+Output: [1,1,2,1,1]
+Explanation: By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,1,2,1,1].
+Example 2:
+Input: nestedList = [1,[4,[6]]]
+Output: [1,4,6]
+Explanation: By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,4,6].
+Constraints:
+The sum of integers in all cases does not exceed 1e5.
+The values of the integers in the nested list are in the range [-1e6, 1e6].
+
+*/
+class NestedIterator {
+public:
+    vector<int> flattenlist;
+    int index;
+    int n;
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        recursivesol(nestedList);
+        index = 0;
+        n = flattenlist.size();
+    }
+    void recursivesol(vector<NestedInteger> nestedList){
+        for(auto i : nestedList){
+            if(i.isInteger()){
+                flattenlist.push_back(i.getInteger());
+            }
+            else{
+                recursivesol(i.getList());
+            }
+        }
+    }
+    int next() {
+        return flattenlist[index++];
+    }
+    
+    bool hasNext() {
+        return index < flattenlist.size();
+    }
+};
