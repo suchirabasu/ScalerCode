a093e8113b33433990ee3f7c1e182f7a3e7f194c
suchirabasu <suchira.basu95@gmail.com>
2023-04-18 11:04:39 +0530

    DP Q5

---
+++ Dynamic Programming/MaximumSumValue.cpp
@@ -0,0 +1,69 @@
+/*
+Q5. Maximum Sum Value
+Problem Description
+You are given an array A of N integers and three integers B, C, and D.
+You have to find the maximum value of A[i]*B + A[j]*C + A[k]*D, where 1 <= i <= j <= k <= N.
+
+Problem Constraints
+1 <= N <= 105
+-10000 <= A[i], B, C, D <= 10000
+
+Input Format
+First argument is an array A
+Second argument is an integer B
+Third argument is an integer C
+Fourth argument is an integer D
+
+Output Format
+Return an Integer S, i.e maximum value of (A[i] * B + A[j] * C + A[k] * D), where 1 <= i <= j <= k <= N.
+
+Example Input
+Input 1:
+ A = [1, 5, -3, 4, -2]
+ B = 2
+ C = 1
+ D = -1
+Input 2:
+ A = [3, 2, 1]
+ B = 1
+ C = -10
+ D = 3
+
+Example Output
+Output 1:
+ 18
+Output 2:
+ -4
+
+Example Explanation
+Explanation 1:
+ If you choose i = 2, j = 2, and k = 3 then we will get
+ A[2]*B + A[2]*C + A[3]*D = 5*2 + 5*1 + (-3)*(-1) = 10 + 5 + 3 = 18
+Explanation 2:
+ If you choose i = 1, j = 3, and k = 3 then we will get
+ A[1]*B + A[3]*C + A[3]*D = (3*1) + (-10*1) + (3*1) = 3 - 10 + 3 = -4
+
+*/
+int Solution::solve(vector<int> &A, int B, int C, int D) {
+    int n = A.size();
+
+    vector<int>v(3,0);
+    v[0]=B;v[1]=C;v[2]=D;
+
+    vector<int>prev(3,0),curr(3,0);
+ 
+    for(int i=0;i<n;i++)
+    {
+        for(int ind=0;ind<3;ind++)
+        {
+            int val1 = A[i]*v[ind];
+            if(ind-1>=0) val1 += curr[ind-1];
+            int val2 = -1e9;
+            if(i-1>=0) val2 = prev[ind];
+
+           curr[ind] = max(val1,val2);
+        }
+        prev=curr;
+    }
+    return prev[2];
+}
