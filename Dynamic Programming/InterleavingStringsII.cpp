e501866979e6f545b837c1baef6ad52ef9c5eda2
suchirabasu <suchira.basu95@gmail.com>
2023-05-23 10:58:06 +0530

    DP Q34

---
+++ Dynamic Programming/InterleavingStringsII.cpp
@@ -0,0 +1,66 @@
+/*
+Q34. Interleaving Strings
+Problem Description
+Given A, B, C find whether C is formed by the interleaving of A and B.
+
+Problem Constraints
+1 <= length(A), length(B) <= 100
+1 <= length(C) <= 200
+
+Input Format
+The first argument of input contains a string, A.
+The second argument of input contains a string, B.
+The third argument of input contains a string, C.
+
+Output Format
+Return 1 if string C is formed by interleaving of A and B else 0.
+
+Example Input
+Input 1:
+ A = "aabcc"
+ B = "dbbca"
+ C = "aadbbcbcac"
+Input 2:
+ A = "aabcc"
+ B = "dbbca"
+ C = "aadbbbaccc"
+
+Example Output
+Output 1:
+ 1
+Output 2:
+ 0
+
+Example Explanation
+Explanation 1:
+ "aa" (from A) + "dbbc" (from B) + "bc" (from A) + "a" (from B) + "c" (from A)
+Explanation 2:
+ It is not possible to get C by interleaving A and B.
+
+*/
+int Solution::isInterleave(string A, string B, string C) {
+
+    int a = A.length();
+    int b = B.length();
+    int c = C.length();
+
+    bool dp[a + 1][b + 1];
+    dp[0][0] = true;
+
+    for(int i = 1; i <= a; i++)
+        dp[i][0] = (C[i - 1] == A[i - 1] && dp[i - 1][0] == true) ? true : false;
+   
+    for(int i = 1; i <= b; i++)
+        dp[0][i] = (C[i - 1] == B[i - 1] && dp[0][i - 1] == true) ? true : false;
+   
+    for(int i = 1; i <= a; i++)
+        for(int j = 1; j <= b; j++)
+            dp[i][j] = (C[i + j - 1] == A[i - 1] && dp[i - 1][j] == true)
+                    ? true
+                    : (C[i + j - 1] == B[j - 1] && dp[i][j - 1] == true)
+                    ? true
+                    : false;
+   
+    return dp[a][b];
+}
+
